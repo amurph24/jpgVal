876634acd9c55bae9578f0c65cfdb79d85c6d0f5
amurph24 <aidanpmurphy@hotmail.com>
2024-03-29 19:25:09 -0700

    validate JPG file working

--- src/main.c
+++ src/main.c
@@ -5,8 +5,61 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int isJPG(char *path) {
+#define MAX_BYTES 20000000
+#define FF 255
+#define D8 216
+#define D9 217
+
+
+int isFileJPG(FILE *file, char *path) {
+	int counter = 0;
+	unsigned char fileBytes[2]; 
+	fileBytes[0] = getc(file);
+	fileBytes[1] = getc(file);
+
+	// to check for file end
+	int temp = getc(file);
+
+	// validate first two bytes of file
+	if (fileBytes[0] != FF || fileBytes[1] != D8) {
+		return 0;
+	}
+	
+	// move "buffer" over bytes
+	do {
+		if (counter >= MAX_BYTES) {
+			printf("%20s exceeds max file size of: %d bytes\n", path, MAX_BYTES);
+			return 0;
+		}
+		// move "buffer" forward
+		fileBytes[0] = fileBytes[1];
+		fileBytes[1] = temp;
+		
+		// get next byte
+		temp = getc(file);
+	} while (temp != EOF); 
+
+	// validate last two bytes of file
+	if (fileBytes[0] != FF || fileBytes[1] != D9) {
+		return 0;
+	}
+	
+	printf("%20s is a valid jpg file\n", path);
+	return 1;
+}
+
+int isPathJPG(char *path) {
+	// pass file to checker
 	FILE *file = fopen(path, "rb");
+	int isJPG = isFileJPG(file, path);
+	fclose(file);
+
+	// throw errors
+	if (!isJPG) {
+		printf("%20s is not a valid jpg file\n", path);
+	}
+
+	return isJPG;
 }
 
 int validateFiles(int argc, char **argv) {
@@ -28,14 +81,14 @@ int validateFiles(int argc, char **argv) {
 	DIR *dir = opendir(argv[1]);
 	if (!dir) {
 		printf("'%s' is not a dir\n", argv[1]);
-		// isJPG(argv[1]);
+		isPathJPG(argv[1]);
 		return 0;
 	}
 
 	printf("'%s' is a dir, checking contents...\n", argv[1]);
 		// for (...)
 		// {
-		// *recursion*
+		// *recursion call*
 		// }
 	// END recursion
 	return 0;
